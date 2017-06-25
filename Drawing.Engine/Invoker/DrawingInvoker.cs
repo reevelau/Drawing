1388fcc6ac2ef249ce4343c1a598b2da816daf29
Reeve <reeve@Reeves-MacBook-Pro.local>
2017-06-26 01:09:27 +0800

    minor change

--- Drawing.Engine/Invoker/DrawingInvoker.cs
+++ Drawing.Engine/Invoker/DrawingInvoker.cs
@@ -6,12 +6,10 @@ namespace Drawing.Engine.Invoker
 {
     public class DrawingInvoker : IDrawingInvoker
     {
-        protected List<ICanvasCommand> History {get;set;} = new List<ICanvasCommand>();
         public  void StoreAndExecute(ICanvasCommand command)
         {
             try{
                 command.Execute();
-                History.Add(command);
                 OnExecuteSuccess();
             }
             catch(Exception e)
